2369efc81ca6c7625a509331fdf986c97f809d57
Penryn <15158052130@163.com>
2024-10-21 17:46:42 +0800

    fix: 修复换绑邮箱成功还发送验证码的问题

--- app/controllers/emailController/emailReSetController.go
+++ app/controllers/emailController/emailReSetController.go
@@ -1,13 +1,9 @@
 package emailController
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
-	"math/rand"
 	"net/http"
-	"time"
-	"usercenter/app/services/emailService"
 	"usercenter/app/services/userService"
 	"usercenter/app/utility"
 )
@@ -44,15 +40,5 @@ func EmailReset(c *gin.Context) {
 		return
 	}
 
-	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-	vcode := fmt.Sprintf("%06v", rnd.Int31n(1000000))
-
-	emailService.SendEmail(data.Email, vcode)
-	err = userService.CreateUserInRedis("", data.Email, "", vcode, 0, 0)
-	if err != nil {
-		log.Println(err)
-		utility.JsonResponseInternalServerError(c)
-		return
-	}
 	utility.JsonResponse(http.StatusOK, "OK", nil, c)
 }
