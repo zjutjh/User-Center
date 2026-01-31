4991187612c9087e1a33baeca45a941c9ed41ce9
cbluebird <crk2778355976@163.com>
2026-01-31 19:39:40 +0800

    pref

--- handler/user.go
+++ handler/user.go
@@ -2,95 +2,37 @@ package handler
 
 import (
 	"context"
-	"errors"
-
-	"gorm.io/gorm"
-
 	userv1 "github.com/zjutjh/User-Center/api/user/v1alpha1"
-	"github.com/zjutjh/User-Center/pkg/expection"
-	"github.com/zjutjh/User-Center/pkg/util"
 	userService "github.com/zjutjh/User-Center/service/user"
-	"github.com/zjutjh/WeJH-SDK/oauth"
-	"github.com/zjutjh/WeJH-SDK/oauth/oauthException"
 )
 
 type UserHandler struct {
 	userv1.UnimplementedUserCenterServiceServer
+	userService *userService.UserService
 }
 
 func NewUserHandler() *UserHandler {
-	return &UserHandler{}
+	return &UserHandler{
+		userService: userService.NewUserService(),
+	}
 }
 
-func (u *UserHandler) Register(ctx context.Context, req *userv1.RegisterRequest) (*userv1.Response, error) {
-	if err := userService.CheckStudentBySIDAndIID(ctx, req.StudentId, req.Iid); err != nil {
-		return expection.UserNotFound.ToResponse()
-	}
-	if err := userService.CreateUser(ctx, req.Password, req.Email, req.StudentId); err != nil {
-		if errors.Is(err, expection.UserAlreadyExit) {
-			return expection.UserAlreadyExit.ToResponse()
-		}
-		return expection.Unknown.ToResponse()
-	}
-	return util.ResponseSuccess(nil)
+func (h *UserHandler) Register(ctx context.Context, req *userv1.RegisterRequest) (*userv1.Response, error) {
+	return h.userService.Register(ctx, req)
 }
 
-func (u *UserHandler) Login(ctx context.Context, req *userv1.LoginRequest) (*userv1.Response, error) {
-	_, err := userService.Login(ctx, req.StudentId, req.Password)
-	if err != nil {
-		return expection.Unknown.ToResponse()
-	}
-	return util.ResponseSuccess(nil)
+func (h *UserHandler) Login(ctx context.Context, req *userv1.LoginRequest) (*userv1.Response, error) {
+	return h.userService.Login(ctx, req)
 }
 
-func (u *UserHandler) ResetPassword(ctx context.Context, req *userv1.ResetPasswordRequest) (*userv1.Response, error) {
-	if err := userService.CheckStudentBySIDAndIID(ctx, req.StudentId, req.Iid); err != nil {
-		return expection.UserNotFound.ToResponse()
-	}
-	if err := userService.UpdateUserPassword(ctx, req.StudentId, req.Password); err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return expection.UserNotExit.ToResponse()
-		}
-		return expection.Unknown.ToResponse()
-	}
-	return util.ResponseSuccess(nil)
+func (h *UserHandler) ResetPassword(ctx context.Context, req *userv1.ResetPasswordRequest) (*userv1.Response, error) {
+	return h.userService.ResetPassword(ctx, req)
 }
 
-func (u *UserHandler) Delete(ctx context.Context, req *userv1.DeleteRequest) (*userv1.Response, error) {
-	if err := userService.CheckStudentBySIDAndIID(ctx, req.StudentId, req.Iid); err != nil {
-		return expection.UserNotFound.ToResponse()
-	}
-	if err := userService.Delete(ctx, req.StudentId); err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return expection.UserNotExit.ToResponse()
-		}
-		return expection.Unknown.ToResponse()
-	}
-	return util.ResponseSuccess(nil)
+func (h *UserHandler) Delete(ctx context.Context, req *userv1.DeleteRequest) (*userv1.Response, error) {
+	return h.userService.Delete(ctx, req)
 }
 
-func (u *UserHandler) OauthLogin(ctx context.Context, req *userv1.LoginRequest) (*userv1.Response, error) {
-	_, userInfo, err := oauth.GetUserInfo(req.StudentId, req.Password)
-	if err != nil {
-		switch {
-		case errors.Is(err, oauthException.ClosedError):
-			return expection.ClosedError.ToResponse()
-		case errors.Is(err, oauthException.WrongPassword):
-			return expection.WrongPassword.ToResponse()
-		case errors.Is(err, oauthException.NotActivatedError):
-			return expection.NotActivatedError.ToResponse()
-		case errors.Is(err, oauthException.WrongAccount):
-			return expection.WrongAccount.ToResponse()
-		case errors.Is(err, oauthException.OtherError):
-			return expection.Unknown.ToResponse()
-		}
-	}
-	return util.ResponseSuccess(map[string]interface{}{
-		"name":         userInfo.Name,
-		"studentId":    userInfo.StudentID,
-		"userType":     userInfo.UserType,
-		"userTypeDesc": userInfo.UserTypeDesc,
-		"gender":       userInfo.Gender,
-		"avatar":       userInfo.Avatar,
-	})
+func (h *UserHandler) OauthLogin(ctx context.Context, req *userv1.LoginRequest) (*userv1.Response, error) {
+	return h.userService.OauthLogin(ctx, req)
 }
--- service/user/userSerivce.go
+++ service/user/userSerivce.go
@@ -3,72 +3,126 @@ package userService
 import (
 	"context"
 	"errors"
+	"gorm.io/gorm"
 	"log/slog"
 	"time"
 
+	userv1 "github.com/zjutjh/User-Center/api/user/v1alpha1"
+	"github.com/zjutjh/WeJH-SDK/oauth"
+	"github.com/zjutjh/WeJH-SDK/oauth/oauthException"
+
 	"github.com/zjutjh/User-Center/dao"
-
-	"gorm.io/gorm"
-
 	"github.com/zjutjh/User-Center/dao/model"
 	"github.com/zjutjh/User-Center/pkg/expection"
 	"github.com/zjutjh/User-Center/pkg/util"
 )
 
-func CheckStudentBySIDAndIID(ctx context.Context, studentID string, iid string) error {
+type UserService struct {
+}
+
+func NewUserService() *UserService {
+	return &UserService{}
+}
+
+func (u *UserService) Register(ctx context.Context, req *userv1.RegisterRequest) (*userv1.Response, error) {
+	if err := u.checkStudentBySIDAndIID(ctx, req.StudentId, req.Iid); err != nil {
+		return expection.UserNotFound.ToResponse()
+	}
+	user, err := dao.GetUserByStudentId(ctx, req.StudentId)
+	if user != nil {
+		return expection.UserAlreadyExit.ToResponse()
+	}
+	if err != nil {
+		slog.Error("failed to get user by student id: %v", err)
+		return expection.Unknown.ToResponse()
+	}
+	pass := util.Encryrpt(req.Password)
+	user = &model.User{
+		Password:   pass,
+		StudentID:  req.StudentId,
+		Email:      req.Email,
+		CreateTime: time.Now(),
+	}
+	if err := dao.CreateUser(ctx, user); err != nil {
+		slog.Error("failed to create user by student id: %v", err)
+		return expection.Unknown.ToResponse()
+	}
+	return util.ResponseSuccess(nil)
+}
+
+func (u *UserService) Login(ctx context.Context, req *userv1.LoginRequest) (*userv1.Response, error) {
+	user, err := dao.GetUserByStudentId(ctx, req.StudentId)
+	if err != nil {
+		return nil, expection.Unknown
+	}
+	if user == nil {
+		return nil, expection.UserNotExit
+	}
+	if user.Password != util.Encryrpt(req.Password) {
+		return nil, expection.AuthError
+	}
+	return util.ResponseSuccess(nil)
+}
+
+func (u *UserService) ResetPassword(ctx context.Context, req *userv1.ResetPasswordRequest) (*userv1.Response, error) {
+	if err := u.checkStudentBySIDAndIID(ctx, req.StudentId, req.Iid); err != nil {
+		return expection.UserNotFound.ToResponse()
+	}
+	user, err := dao.GetUserByStudentId(ctx, req.StudentId)
+	if err != nil {
+		return expection.Unknown.ToResponse()
+	}
+	user.Password = util.Encryrpt(req.Password)
+	if err := dao.UpdateUserPassword(ctx, user); err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return expection.UserNotExit.ToResponse()
+		}
+		slog.Error("failed to update user password: %v", err)
+		return expection.Unknown.ToResponse()
+	}
+	return util.ResponseSuccess(nil)
+}
+
+func (u *UserService) Delete(ctx context.Context, req *userv1.DeleteRequest) (*userv1.Response, error) {
+	if err := u.checkStudentBySIDAndIID(ctx, req.StudentId, req.Iid); err != nil {
+		return expection.UserNotFound.ToResponse()
+	}
+	if err := dao.DeleteUserByStudentId(ctx, req.StudentId); err != nil {
+		return expection.Unknown.ToResponse()
+	}
+	return util.ResponseSuccess(nil)
+}
+
+func (u *UserService) OauthLogin(ctx context.Context, req *userv1.LoginRequest) (*userv1.Response, error) {
+	_, userInfo, err := oauth.GetUserInfo(req.StudentId, req.Password)
+	if err != nil {
+		switch {
+		case errors.Is(err, oauthException.ClosedError):
+			return expection.ClosedError.ToResponse()
+		case errors.Is(err, oauthException.WrongPassword):
+			return expection.WrongPassword.ToResponse()
+		case errors.Is(err, oauthException.NotActivatedError):
+			return expection.NotActivatedError.ToResponse()
+		case errors.Is(err, oauthException.WrongAccount):
+			return expection.WrongAccount.ToResponse()
+		case errors.Is(err, oauthException.OtherError):
+			return expection.Unknown.ToResponse()
+		}
+	}
+	return util.ResponseSuccess(map[string]interface{}{
+		"name":         userInfo.Name,
+		"studentId":    userInfo.StudentID,
+		"userType":     userInfo.UserType,
+		"userTypeDesc": userInfo.UserTypeDesc,
+		"gender":       userInfo.Gender,
+		"avatar":       userInfo.Avatar,
+	})
+}
+
+func (u *UserService) checkStudentBySIDAndIID(ctx context.Context, studentID string, iid string) error {
 	student, err := dao.GetStudentByStudentID(ctx, studentID)
-	if err != nil || student.IDCard != iid {
+	if err != nil || student == nil || student.IDCard != iid {
 		return expection.UserNotFound
 	}
 	return nil
-}
-
-func CreateUser(ctx context.Context, password, email, sid string) error {
-	user, err := dao.GetUserByStudentId(ctx, sid)
-	if user != nil {
-		return expection.UserAlreadyExit
-	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
-		slog.Error("failed to get user by student id: %v", err)
-		return expection.Unknown
-	}
-	pass := util.Encryrpt(password)
-	user = &model.User{
-		Password:   pass,
-		StudentID:  sid,
-		Email:      email,
-		CreateTime: time.Now(),
-	}
-	return dao.CreateUser(ctx, user)
-}
-
-func UpdateUserPassword(ctx context.Context, studentId, password string) error {
-	user, err := dao.GetUserByStudentId(ctx, studentId)
-	if err != nil {
-		return err
-	}
-	pass := util.Encryrpt(password)
-	user.Password = pass
-	return dao.UpdateUserPassword(ctx, user)
-}
-
-func Delete(ctx context.Context, stuID string) error {
-	user, err := dao.GetUserByStudentId(ctx, stuID)
-	if err != nil {
-		return err
-	}
-	return dao.DeleteUserByStudentId(ctx, user.StudentID)
-}
-
-func Login(ctx context.Context, studentID, password string) (*model.User, error) {
-	user, err := dao.GetUserByStudentId(ctx, studentID)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, expection.UserNotExit
-		}
-		return nil, expection.Unknown
-	}
-	if user.Password != util.Encryrpt(password) {
-		return nil, expection.AuthError
-	}
-	return user, nil
 }
