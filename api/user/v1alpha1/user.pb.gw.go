bfab15418a326844e33068fb21515dfe964a0ffb
cbluebird <crk2778355976@163.com>
2026-01-31 21:20:09 +0800

    fix go mod error

--- api/user/v1alpha1/user.pb.gw.go
+++ api/user/v1alpha1/user.pb.gw.go
@@ -170,6 +170,33 @@ func local_request_UserCenterService_OauthLogin_0(ctx context.Context, marshaler
 	return msg, metadata, err
 }
 
+func request_UserCenterService_HealthyCheck_0(ctx context.Context, marshaler runtime.Marshaler, client UserCenterServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq HealthyCheckRequest
+		metadata runtime.ServerMetadata
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
+	}
+	msg, err := client.HealthyCheck(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+}
+
+func local_request_UserCenterService_HealthyCheck_0(ctx context.Context, marshaler runtime.Marshaler, server UserCenterServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq HealthyCheckRequest
+		metadata runtime.ServerMetadata
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	msg, err := server.HealthyCheck(ctx, &protoReq)
+	return msg, metadata, err
+}
+
 // RegisterUserCenterServiceHandlerServer registers the http handlers for service UserCenterService to "mux".
 // UnaryRPC     :call UserCenterServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -275,6 +302,26 @@ func RegisterUserCenterServiceHandlerServer(ctx context.Context, mux *runtime.Se
 			return
 		}
 		forward_UserCenterService_OauthLogin_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
+	mux.Handle(http.MethodPost, pattern_UserCenterService_HealthyCheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/zjut.jh.api.user.v1alpha1.UserCenterService/HealthyCheck", runtime.WithHTTPPathPattern("/api/healthy"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_UserCenterService_HealthyCheck_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_UserCenterService_HealthyCheck_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -401,6 +448,23 @@ func RegisterUserCenterServiceHandlerClient(ctx context.Context, mux *runtime.Se
 		}
 		forward_UserCenterService_OauthLogin_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
+	mux.Handle(http.MethodPost, pattern_UserCenterService_HealthyCheck_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/zjut.jh.api.user.v1alpha1.UserCenterService/HealthyCheck", runtime.WithHTTPPathPattern("/api/healthy"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_UserCenterService_HealthyCheck_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_UserCenterService_HealthyCheck_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
 	return nil
 }
 
@@ -410,6 +474,7 @@ var (
 	pattern_UserCenterService_ResetPassword_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "repass"}, ""))
 	pattern_UserCenterService_Delete_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "del"}, ""))
 	pattern_UserCenterService_OauthLogin_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "oauth"}, ""))
+	pattern_UserCenterService_HealthyCheck_0  = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "healthy"}, ""))
 )
 
 var (
@@ -418,4 +483,5 @@ var (
 	forward_UserCenterService_ResetPassword_0 = runtime.ForwardResponseMessage
 	forward_UserCenterService_Delete_0        = runtime.ForwardResponseMessage
 	forward_UserCenterService_OauthLogin_0    = runtime.ForwardResponseMessage
+	forward_UserCenterService_HealthyCheck_0  = runtime.ForwardResponseMessage
 )
